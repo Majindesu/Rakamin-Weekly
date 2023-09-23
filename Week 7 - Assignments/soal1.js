fce1e4aebd072658665f96da6d23223cdb7a69c0
majindesu <rwroguesaint@gmail.com>
2023-09-23 13:57:48 +0700

    update ulang semua folder penugasan hingga Week 7, tambahan baru folder Week 7 berisi 5 file untuk homework pada week 7.

---
+++ Week 7 - Assignments/soal1.js
@@ -0,0 +1,8 @@
+//pendefinisian fungsi local module
+const calculate = require('./Calculate.js');
+
+//pemanggilan local module
+console.log(calculate.area(15))
+console.log(calculate.area(15, 20))
+console.log(calculate.circ(15))
+console.log(calculate.circ(15, 20))
