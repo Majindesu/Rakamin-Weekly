a16be6ca464762be4448f99a0054876569f8d392
majindesu <rwroguesaint@gmail.com>
2023-09-23 14:03:21 +0700

    deleted all old files

--- Week 7 - Assignments/Calculate.js
+++
@@ -1,4 +0,0 @@
-//Function menghitung luas persegi & persegi panjang
-exports.area = (areaLength, areaWidth = areaLength) => areaLength * areaWidth
-//Function menghitung keliling persegi & pergsegi panjang
-exports.circ = (areaLength, areaWidth = areaLength) => (areaLength + areaWidth) * 2
--- Week 7 - Assignments/soal1.js
+++
@@ -1,8 +0,0 @@
-//pendefinisian fungsi local module
-const calculate = require('./Calculate.js');
-
-//pemanggilan local module
-console.log(calculate.area(15))
-console.log(calculate.area(15, 20))
-console.log(calculate.circ(15))
-console.log(calculate.circ(15, 20))
