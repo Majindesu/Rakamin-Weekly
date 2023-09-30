b2ef22b14dabb7692f01e591e45d7e4843dfe4b5
majindesu <rwroguesaint@gmail.com>
2023-09-30 08:22:46 +0700

    Folder berisikan sql dan javascript untuk seeding kedalam database

---
+++ Week 8 - Assignments/db/seeding.sql
@@ -0,0 +1,6 @@
+INSERT INTO actor (actor_id, first_name, last_name, last_update) VALUES
+    (201, 'Charles', 'White', '2023-09-30 06:29:22'),
+    (202, 'Imane', 'Anys', '2023-09-30 06:29:22'),
+    (203, 'Anthony', 'Padilla', '2023-09-30 06:29:22'),
+    (204, 'Ian', 'Hecox', '2023-09-30 06:29:22'),
+    (205, 'Connor', 'Colquhoun', '2023-09-30 06:29:22');
